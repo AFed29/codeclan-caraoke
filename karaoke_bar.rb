3b3c676808719c6950b85a1886b2d8e729a1a31a
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-02-11 15:51:55 +0000

    Added a method for a guest entering the bar

--- karaoke_bar.rb
+++ karaoke_bar.rb
@@ -1,11 +1,25 @@
 class KaraokeBar
-  attr_reader(:name, :entry_fee, :rooms)
+  attr_reader(:name, :till, :entry_fee, :rooms, :songs)
 
-  def initialize(name, entry_fee, rooms = [], songs = [])
+  def initialize(name, till, entry_fee, rooms = [], songs = [])
     @name = name
+    @till = till
     @entry_fee = entry_fee
     @rooms = rooms
     @songs = songs
+  end
+
+  def add_money_to_till()
+    @till += @entry_fee
+  end
+
+  def guest_checking_into_room(guest, chosen_room)
+    @rooms.each do |room|
+      if (room == chosen_room && room.guests().count < room.size())
+        room.add_guest_to_room(guest)
+        add_money_to_till()
+      end
+    end
   end
 
 end
--- specs/karaoke_bar_spec.rb
+++ specs/karaoke_bar_spec.rb
@@ -6,22 +6,58 @@ require_relative('../song')
 
 class TestKaraokeBar < MiniTest::Test
   def setup
-    room1 = Room.new("MJ", 5)
+    @guest1 = Guest.new("John", 25.00)
+    @guest2 = Guest.new("Sam", 25.00)
+    @guest3 = Guest.new("Alan", 25.00)
+    @guest4 = Guest.new("Steven", 25.00)
 
-    @bar = KaraokeBar.new("The Mercury", 10.00, [room1])
+    @group1 = [@guest1, @guest2, @guest3]
+
+    @room1 = Room.new("MJ", 5)
+
+    song1 = Song.new("Bohemian Rhapsody", "Queen")
+    song2 = Song.new("I'm Gonna Be (500 Miles)", "The Proclaimers")
+    song3 = Song.new("Don't Stop Believing", "Journey")
+
+    @bar = KaraokeBar.new("The Mercury", 200.00, 10.00, [@room1], [song1, song2, song3])
   end
 
   def test_check_bar_has_name()
     assert_equal("The Mercury", @bar.name())
   end
 
+  def test_check_bar_has_till()
+    assert_equal(200.00, @bar.till())
+  end
+
   def test_check_bar_has_entry_fee()
     assert_equal(10.00, @bar.entry_fee())
   end
 
-  def test_check_bar_has_room()
-    assert_equal(1, @bar.rooms().count())    
+  def test_check_bar_has_rooms()
+    assert_equal(1, @bar.rooms().count())
   end
 
+  def test_bar_has_songs()
+    assert_equal(3, @bar.songs().count())
+  end
+
+  def test_add_money_to_till()
+    @bar.add_money_to_till()
+    assert_equal(210.00, @bar.till())
+  end
+
+  def test_single_guest_checking_into_room__enough_space()
+    @bar.guest_checking_into_room(@guest1, @room1)
+    assert_equal(1, @room1.guests().count())
+    assert_equal(210.00, @bar.till())
+  end
+
+  def test_single_guest_checking_into_room__no_space()
+    room = Room.new("Sam & Dave", 3, [@guest1, @guest2, @guest3])
+    @bar.guest_checking_into_room(@guest4, room)
+    assert_equal(3, room.guests().count())
+    assert_equal(200.00, @bar.till)
+  end
 
 end
